feafcc8159e6f131e6c14bcc01bde6a4a34b6e52
cn-zxcv <1032980965@qq.com>
2024-11-13 11:05:51 +0800

    nvim 配置更新; <C-/> 注释切换; 禁止鼠标操作

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -73,7 +73,7 @@ require("lazy").setup({
     -- 文件搜索
     {
         "nvim-telescope/telescope.nvim",
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-live-grep-args.nvim' },
         config = function()
 			require("config.nvim-telescope")
         end,
